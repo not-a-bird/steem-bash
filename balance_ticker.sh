90855e9ac8389317a80b52ee21500d8a982439c4
Evilest Fiend <evilest.fiend@gmail.com>
2018-01-19 00:05:24 -0500

    Many improvements:
     * added argument parsing for tunable output
       User can choose
       + display full account value
       + display balances (sbd, steem, savings) [defualt]
       + display SP
       + display pending payouts
     * made ticker format optional (can do one-shot and exit)
     * added ability to display other output currencies (try LTC, for example)
       This is used for:
       + full account value option
       + pending payout option
     * added ability to specify the service endpoint (future proofing)
     * minimized external service calls (speedup)

--- balance_ticker.sh
+++ balance_ticker.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 ##
-# This script fetches the total worth of the specified users and displays it in the style of a stock ticker.
+# Display account balances and other information.  Optionally as a stock ticker.
+# Usage: 
+#    ${0} [-b] [-c CURRENCY] [-e RPC_ENDPOINT] [-s] [-t] [-w] [-p] [-h]"
+#
+#- b show balances (sbd, steem, savings) [default when no options specified]
+#- c CURRENCY (default is USD)
+#- e specify service node endpoint
+#- h show help
+#- p include pending payouts in output
+#- s include SP in output
+#- t enable stock ticker output
+#- w include total account worth in output
 
 trap "tput cnorm" exit
 
@@ -13,40 +24,107 @@ fi
 . "${WHEREAMI}"/functions.sh
 
 ##
-# Get a ticker of various account $$ information.
-get_ticker_string(){
-    local WHOM=${1}
-    local WHERE=$(mktemp)
-    local SUCCESS=0
-    local CURRENCY=${2:-USD}
+# Display help message.
+usage(){
+    cat << EOF
+Usage: 
+    ${0} [-b] [-c CURRENCY] [-e RPC_ENDPOINT] [-s] [-t] [-w] [-p] [-h] <USER> [USER ...]"
 
-    if rpc_get_accounts "${WHOM}" | jq '.[0]' > "${WHERE}" ; then
-        local PRICES=$(get_prices "STEEM SBD" "${CURRENCY}")
-        local STEEMV=$(jq ".STEEM.${CURRENCY}" <<< $PRICES)
-        local SBDV=$(jq ".SBD.${CURRENCY}" <<< $PRICES)
-        local BALANCE=$(jq '.balance' < "${WHERE}" | cut -f2 -d'"' |  cut -f1 -d" ")
-        local SBD_BALANCE=$(jq '.sbd_balance' < "${WHERE}" | cut -f2 -d'"'| cut -f1 -d" ")
-        local VESTING_SHARES=$(jq '.vesting_shares' < "${WHERE}" | cut -f2 -d'"'| cut -f1 -d" ")
-        local STEEM_SAVINGS=$(jq '.savings_balance' < "${WHERE}" | cut -f2 -d'"'| cut -f1 -d" ")
-        local STEEM_POWER=$(get_steempower_for_vests "$VESTING_SHARES")
-        local STEEMS=$(math "${STEEM_POWER}+${STEEM_SAVINGS}+${BALANCE}" 2)
-        local BANK=$(math "(${BALANCE}+${STEEM_POWER}+${STEEM_SAVINGS}) * ${STEEMV} + ${SBD_BALANCE} * ${SBDV}")
-        local BANKFMT=$(printf "%'0.2f" "${BANK}")
-        echo "${WHOM}: ${BANKFMT} ${CURRENCY} [$(math "${SBDV}*${SBD_BALANCE}" 2) ${CURRENCY} (${SBD_BALANCE} SBD at ${SBDV} ${CURRENCY}) STEEM: $(math "${STEEMS} * ${STEEMV}" 2) ${CURRENCY} (${STEEMS} STEEM at ${STEEMV} ${CURRENCY})]"
-    fi
-    rm "${WHERE}"
+Get and display balance information about the specified user.
+
+- b show balances (sbd, steem, savings) [default when no options specified]
+- c CURRENCY (default is USD)
+- e specify service node endpoint
+- h show (this) help
+- p include pending payouts in output
+- s include SP in output
+- t enable stock ticker output
+- w include total account worth in output
+EOF
 }
 
+CURRENCY=USD
+while getopts ":c:e:bhstwp" OPT; do
+    case "${OPT}" in
+        b )
+        BALANCE=YES
+        ;;
+        c )
+        CURRENCY=${OPTARG}
+        ;;
+        e )
+        RPC_ENDPOINT=${OPTARG}
+        ;;
+        s )
+        SP=YES
+        ;;
+        t )
+        TICKER=YES
+        ;;
+        w )
+        WORTH=YES
+        ;;
+        p )
+        PENDING=YES
+        ;;
+        *) usage
+            exit
+        ;;
+    esac
+done
+shift $((OPTIND -1))
 
+if [ -z "${BALANCE}${SP}${WORTH}${PENDING}" ] ; then
+    BALANCE=YES
+fi
 if [ -z "${1}" ] ; then
     error "No user specified!  Specify one or more users to see their account worth in a ticker."
+    usage
 else
+    WHERE=$(mktemp)
     while true; do
-        echo -ne '\r.'
+        if [ ! -z "${TICKER}" ] ; then
+            echo -ne '\r.'
+        fi
         TICKERINFO=
         for USER in $@ ; do
-            TICKERINFO=${TICKERINFO}"$(get_ticker_string "${USER}")"
+            TICKERINFO="${TICKERINFO}${USER} "
+            if rpc_get_accounts "${USER}" | jq '.[0]' > "${WHERE}" ; then
+                STEEM_BALANCE=$(jq '.balance' < "${WHERE}" | cut -f2 -d'"'| cut -f1 -d" ")
+                SBD_BALANCE=$(jq '.sbd_balance' < "${WHERE}" | cut -f2 -d'"'| cut -f1 -d" ")
+                VESTING_SHARES=$(jq '.vesting_shares' < "${WHERE}" | cut -f2 -d'"'| cut -f1 -d" ")
+                STEEM_SAVINGS=$(jq '.savings_balance' < "${WHERE}" | cut -f2 -d'"'| cut -f1 -d" ")
+                STEEM_POWER=$(get_steempower_for_vests "$VESTING_SHARES")
+                PRICES=$(get_prices "STEEM SBD" "${CURRENCY}")
+                SBDV=$(jq ".SBD.${CURRENCY}" <<< $PRICES)
+                if [ ! -z "${WORTH}" ] ; then
+                    STEEMV=$(jq ".STEEM.${CURRENCY}" <<< $PRICES)
+                    printf -v BANKFMT "%'0.2f" $(math "${STEEM_BALANCE}*${STEEMV}+${SBD_BALANCE}*${SBDV}+${STEEM_POWER}*${STEEMV}")
+                    TICKERINFO="${TICKERINFO} worth: ${BANKFMT} ${CURRENCY}"
+                fi
+                if [ ! -z "${BALANCE}" ] ; then
+                    TICKERINFO="${TICKERINFO} ${STEEM_BALANCE} STEEM"
+                    TICKERINFO="${TICKERINFO} ${SBD_BALANCE} SBD"
+                    TICKERINFO="${TICKERINFO} ${STEEM_SAVINGS} Savings"
+                fi
+                if [ ! -z "${SP}" ] ; then
+                    TICKERINFO="${TICKERINFO} ${STEEM_POWER} SP"
+                fi
+                if [ ! -z "${PENDING}" ] ; then
+                    PAYOUT=$(get_payout "${USER}")
+                    TICKERINFO="${TICKERINFO} pending payout: ${PAYOUT} SBD (${CURRENCY}: $(math "${SBDV}*${PAYOUT}"))"
+                fi
+            fi
+
+            if [ -z "${TICKER}" ] ; then
+                printf -v TICKERINFO "%s\n" "${TICKERINFO}"
+            fi
         done
-        tickline "${TICKERINFO}"
+        if [ ! -z "${TICKER}" ] ; then
+            tickline "${TICKERINFO}"
+        else
+            printf "${TICKERINFO}"
+            exit 0
+        fi
     done
 fi
